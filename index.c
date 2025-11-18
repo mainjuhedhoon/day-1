854d7825a79447c70d9589e52567fa1310531cd1
juhed-1701 <juhedmultani85@gmail.com>
2025-11-18 16:12:59 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+
+printf("================your results===================================== \n");
+printf("students report card managment system \n");
+printf("name: komal agrval \n");
+printf("roll number: 1234 \n");
+printf("                \n");
+
+printf("subjects \t marks1 \t marks2 \t Total \n");
+printf("ch-102c  \t 15     \t 20     \t 50 \n");
+printf("ch-102  \t 15     \t 20     \t 50 \n");
+printf("ch-102  \t 15     \t 20     \t 50 \n");
+printf("ch-102  \t 15     \t 20     \t 50 \n");
+printf("Total marks:300     \t result=pass");
+
+
+    return 0;
+}
