751ecc4708ae26991692a88f276192d8ae81c8ff
MULTANI JUHED <juhedmultani85@gmail.com>
2026-01-22 09:57:34 +0530

    Add files via upload

---
+++ PROJECT - 11.1/script.js
@@ -0,0 +1,26 @@
+// push function array
+let fruits = ["Apple", "Banana"];
+fruits.push("Mango");
+
+console.log(fruits);
+
+
+// pop function array
+let fruits = ["Apple", "Banana", "Mango"];
+fruits.pop();
+
+console.log(fruits);
+
+//shift function array
+let fruits = ["Apple", "Banana", "Mango"];
+fruits.shift();
+
+console.log(fruits);
+
+
+//unshift function array
+let fruits = ["Banana", "Mango"];
+fruits.unshift("Apple");
+
+console.log(fruits);
+
